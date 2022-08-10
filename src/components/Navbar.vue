8ae3d7054db677751540e645b930164029262bae
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-08-10 20:08:16 +0600

    add

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
           <div class="navbar bg-purple-700 text-white">
             <div class="flex-1">
                 <router-link to='/home' className="btn btn-ghost normal-case text-xl">Mahmudul Islam</router-link>
+                
             </div>
             <div class="flex-none">
            
